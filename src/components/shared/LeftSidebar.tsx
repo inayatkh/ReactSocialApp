a2ef1622ccbc490645db9560a4479e1d3cee2a3e
inayatkh <inayatkh@gmail.com>
2023-12-31 21:10:53 +0500

    lefsiderbar logo change and package react copy to clipboard added'

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -24,7 +24,7 @@ function LeftSidebar() {
       <div className="flex flex-col gap-11">
         <Link to="/" className="flex gap-3 items-center">
           <img
-            src="/assets/images/logo.svg"
+            src="/assets/images/logo-no-background.svg"
             alt="logo"
             width={170}
             height={36}
